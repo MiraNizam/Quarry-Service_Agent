e254fd813aadd33c32b11174d3a9931245879bac
MiraNizam <82273561+MiraNizam@users.noreply.github.com>
2023-01-19 22:05:26 +0300

    Add varaibles in class Orders

--- qs_agent/datacenter/models.py
+++ qs_agent/datacenter/models.py
@@ -1,18 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Orders(models.Model):
+    """
+    Model representing detailed information about parts in orders.
+    """
     is_delivered = models.BooleanField(default=False)
     part_no = models.CharField(max_length=50)
-    eng_description = models.CharField(max_length=100)
-    rus_description = models.CharField(max_length=100)
+    rus_description = models.CharField(max_length=100, null=True, blank=True)
     q_ty = models.IntegerField(default=1)
-    lead_time = models.CharField(max_length=50)
-    etd_eta = models.CharField(max_length=50)
-    delivery_date = models.CharField(max_length=50)
+    lead_time = models.CharField(max_length=50, null=True, blank=True)
+    etd = models.CharField(max_length=50, null=True, blank=True)
+    eta = models.CharField(max_length=50, null=True, blank=True)
+    delivery_date = models.CharField(max_length=50, null=True, blank=True)
     order_id = models.CharField(max_length=50)
     manager = models.CharField(max_length=50)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.DO_NOTHING,
+        null=True,
+        blank=True,
+        verbose_name="Менеджер, которому принадлежит заказ")
 
+    def get_manager(self):
+        return self.manager
 
+    def delivered_orders(self):
+        if self.is_delivered:
+            return self.is_delivered
 
-
+    def get_absolute_url(self):
+        return reverse('manager', kwargs={'user': self.pk})
