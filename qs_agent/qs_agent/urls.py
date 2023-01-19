e311840bd26cece20b0c91fa1fc145520c4860d6
MiraNizam <82273561+MiraNizam@users.noreply.github.com>
2023-01-19 22:08:36 +0300

    Update urls.py

--- qs_agent/qs_agent/urls.py
+++ qs_agent/qs_agent/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from datacenter import views
 
-
 urlpatterns = [
-    path('profile/', views.user_profile, name='user_profile'),
+    path('', views.home, name='home'),
+    path('orders/', views.user_orders, name='user_orders'),
+    path('profile/', views.user_profile, name='profile'),
     path('admin/', admin.site.urls),
-    path('', views.orders_qs, name='orders'),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
