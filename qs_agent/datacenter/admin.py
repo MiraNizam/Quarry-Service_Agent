abe79c0ae7aa7f44f1e5fd42eabd5fd59e275604
MiraNizam <82273561+MiraNizam@users.noreply.github.com>
2023-03-15 17:58:38 +0300

    Register model Orders

--- qs_agent/datacenter/admin.py
+++ qs_agent/datacenter/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Orders
 
-# Register your models here.
+
+@admin.register(Orders)
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = ('order_id', 'manager')
+    list_filter = ('order_id', 'manager')
