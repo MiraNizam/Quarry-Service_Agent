27d9985a72967d7356aeaafebe6a3e840fde9bf2
MiraNizam <82273561+MiraNizam@users.noreply.github.com>
2023-03-15 18:46:31 +0300

    Add block try/except

--- qs_agent/datacenter/views.py
+++ qs_agent/datacenter/views.py
@@ -1,3 +1,5 @@
+from django.utils.datastructures import MultiValueDictKeyError
+
 from .models import Orders
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
@@ -25,6 +27,7 @@ def home(request):
 def temp(request):
     return render(request, "temple.html")
 
+
 @login_required
 def user_profile(request):
     return render(request, "profile.html")
@@ -34,29 +37,24 @@ def user_profile(request):
 def import_excel(request):
     orders = Orders.objects.all()
     orders.delete()
-    if request.method == 'POST' and request.FILES['my_file']:
-        file = request.FILES['my_file']
-        file_storage = FileSystemStorage()
-        filename = file_storage.save(file.name, file)
-        uploaded_file_url = file_storage.url(filename)
-        reading_excel_file = pd.read_excel(filename)
-        db_frame = reading_excel_file
-        for db_frame in db_frame.itertuples():
-            obj = Orders.objects.create(
-                part_no=db_frame.part_no,
-                rus_description=db_frame.rus_description,
-                q_ty=db_frame.q_ty,
-                lead_time=db_frame.lead_time,
-                etd=db_frame.etd,
-                eta=db_frame.eta,
-                delivery_date=db_frame.delivery_date,
-                order_id=db_frame.order_id,
-                manager=db_frame.manager
-            )
-            obj.save()
-        return render(request, 'import.html', {
-            'uploaded_file_url': uploaded_file_url
-        })
-    return render(request, 'import.html', {})
-
-
+    try:
+        if request.method == 'POST' and request.FILES['my_file']:
+            file = request.FILES['my_file']
+            reading_excel_file = pd.read_excel(file)
+            db_frame = reading_excel_file
+            for db_frame in db_frame.itertuples():
+                obj = Orders.objects.create(
+                    part_no=db_frame.part_no,
+                    rus_description=db_frame.rus_description,
+                    q_ty=db_frame.q_ty,
+                    lead_time=db_frame.lead_time,
+                    etd=db_frame.etd,
+                    eta=db_frame.eta,
+                    delivery_date=db_frame.delivery_date,
+                    order_id=db_frame.order_id,
+                    manager=db_frame.manager
+                )
+                obj.save()
+        return render(request, 'import.html', {})
+    except MultiValueDictKeyError:
+        return render(request, 'import.html')
