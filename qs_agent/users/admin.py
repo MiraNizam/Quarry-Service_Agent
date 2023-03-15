a8392355bbe005b0f98bc763c00540295abfe711
MiraNizam <82273561+MiraNizam@users.noreply.github.com>
2023-03-15 17:57:20 +0300

    Delete files

--- qs_agent/users/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from datacenter.models import Orders
-
-
-@admin.register(Orders)
-class OrdersAdmin(admin.ModelAdmin):
-    list_display = ('order_id', 'manager')
-    list_filter = ('order_id', 'manager')
